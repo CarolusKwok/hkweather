e557fb54f82a20bb2ffb18ea1f1a34e7593fcc0e
KwokJunKin <102900899+CarolusKwok@users.noreply.github.com>
2022-08-20 15:38:44 +0800

    V9

--- R/load_z_standard.R
+++ R/load_z_standard.R
@@ -13,19 +13,34 @@
 
 
 
+#Download data structure
+#Main Directory
+# /Data
+# /SATL      #the abbr for data type
+# /SATL_02dc #attributes of the data
+# /2022      #Year
+# /202208    #Year and Month
+# /20220801  #Year and Month and Date
+# /filename...
 
-
+#File name
+#SART_en_20220801_0000.csv
+#SATL_02dc_20220801_0000.png
 
 #Type of data
-#export(load_ltng) = load lighting image from HKO website, fil2
-#export(load_mslp) = load mean sea level pressure from HK gov open data, fil3
+#export(load_gtmp_csv) = load grass temperature data from HK gov open data, fil3
+#export(load_ltng)     = load lighting image from HKO website, fil2
+#export(load_mslp_csv) = load mean sea level pressure from HK gov open data, fil3
 #export(load_rain_csv) = load rainfall nowcast data from HK gov open data, fil3
-#export(load_rain_dy) = load rainfall daily data from HKO, fil2
-#export(load_rain_hr) = load rainfall hourly data from HKO, fil2
-#export(load_rhum) = load relative humidity data from HK gov open data
-#export(load_satl) = load satellite image data from HKO, fil2
-#export(load_temp) = load surface air temperature from HK gov open data, fil3
-#export(load_tide) = load actual tide data from HK gov open data, fil3
-#export(load_wind) = load wind image from HKO, fil2
+#export(load_rain_dy)  = load rainfall daily data from HKO, fil2
+#export(load_rain_hr)  = load rainfall hourly data from HKO, fil2
+#export(load_rhum_csv) = load relative humidity data from HK gov open data, fil3
+#export(load_sart_csv) = load solar radiation data from HK gov open data, fil3
+#export(load_satl)     = load satellite image data from HKO, fil2
+#export(load_temp_csv) = load surface air temperature from HK gov open data, fil3
+#export(load_tide_csv) = load actual tide data from HK gov open data, fil3
+#export(load_wind)     = load wind image from HKO, fil2
 #export(load_wind_csv) = load wind data from HK gov open data, fil3
+#export(load_wxph)     = load weather images from HKO, fil2
+
 
--- R/obj_create.R
+++ R/obj_create.R
@@ -1,7 +1,7 @@
 #' Creates an hkweather obj from source files
 #'
 #' @param rDIR Reading Directory. Which folder should I read to get all the information?
-#' @param type Type of data. Which type of data it is? "tide" or "wind"
+#' @param type Type of data. Which type of data it is? (Only accepts gtmp/ mslp/ rain/ rhum/ sart/ temp/ tide/ wind)
 #'
 #' @return
 #' @export
@@ -9,12 +9,21 @@
 #' @examples obj_create(getwd(), type = "tide")
 obj_create = function(rDIR, type){
   hkweather::hkw_lib()
-
   #Create a list of files to read in the dataframe
   File_list = list()
   for (i in 1:length(rDIR)){
     File_list = append(File_list, list.files(rDIR[i], pattern = "*.csv", full.names = T))
   }
+
+  #Check variables!
+  if(length(File_list) < 1){
+    return(message("ERROR: No csv files present!"))
+  }
+  if(length(type) != 1 | !(type %in% c("gtmp", "mslp", "rain", "rhum", "sart", "temp", "tide", "wind"))){
+    return(message("ERROR: Data type incorrect!"))
+  }
+
+  #Start! Sort the files
   File_list = stringr::str_sort(File_list)
   File_s = tools::file_path_sans_ext(basename(File_list[1]))
   File_e = tools::file_path_sans_ext(basename(File_list[length(File_list)]))
@@ -23,38 +32,113 @@ obj_create = function(rDIR, type){
   df = File_list %>% lapply(fread) %>% rbindlist()
 
   #Rename and Reorder df
-  if(type == "tide"){
-    colnames(df) = c("Station", "Date", "Time", "Height")
+  if(type == "gtmp"){
+    colnames(df) = c("DateTime", "Station", "GTMP")
     df = df %>%
-      mutate(Height = as.numeric(Height)) %>%
+      mutate(Hour = paste0(substr(DateTime, 1, 4),
+                           "-", substr(DateTime, 5, 6),
+                           "-", substr(DateTime, 7, 8),
+                           " ", substr(DateTime, 9, 10))) %>%
+      mutate(Min = paste0("Min_", substr(DateTime, 11, 12))) %>%
+      mutate(GTMP = as.numeric(GTMP)) %>%
+      select(-DateTime) %>%
+      relocate(Station, Hour, Min, .before = everything())
+    #"Station", Hour, Min, "GTMP"
+    colnames(df) = c("Station", "Hour", "Min", "Grass Temperature (°C)")
+  }
+
+  if(type == "mslp"){
+    #Date time,Automatic Weather Station,Mean Sea Level Pressure(hPa)
+    colnames(df) = c("DateTime", "Station", "MSLP")
+    df = df %>%
+      mutate(Hour = paste0(substr(DateTime, 1, 4),
+                           "-", substr(DateTime, 5, 6),
+                           "-", substr(DateTime, 7, 8),
+                           " ", substr(DateTime, 9, 10))) %>%
+      mutate(Min = paste0("Min_", substr(DateTime, 11, 12))) %>%
+      mutate(MSLP = as.numeric(MSLP)) %>%
+      select(-DateTime) %>%
+      relocate(Station, Hour, Min, .before = everything())
+    #Station, Hour, Min, MSLP
+    colnames(df) = c("Station", "Hour", "Min", "Mean Sea Level Pressure (hPa)")
+  }
+
+  if(type == "rain"){
+    return(message("ERROR: rain is not support yet!"))
+  }
+
+  if(type == "rhum"){
+    #Date time,Automatic Weather Station,Relative Humidity(percent)
+    colnames(df) = c("DateTime", "Station", "RHUM")
+    df = df %>%
+      mutate(Hour = paste0(substr(DateTime, 1, 4),
+                           "-", substr(DateTime, 5, 6),
+                           "-", substr(DateTime, 7, 8),
+                           " ", substr(DateTime, 9, 10))) %>%
+      mutate(Min = paste0("Min_", substr(DateTime, 11, 12))) %>%
+      mutate(RHUM = as.numeric(RHUM)) %>%
+      select(-DateTime) %>%
+      relocate(Station, Hour, Min, .before = everything())
+    #Station, Hour, Min, RHUM
+    colnames(df) = c("Station", "Hour", "Min", "Relative Humidity (%)")
+  }
+
+  if(type == "sart"){
+    #Date time,Automatic Weather Station,Global Solar Radiation(watt/square meter),Direct Solar Radiation(watt/square meter),Diffuse Radiation(watt/square meter)
+    colnames(df) = c("DateTime", "Station", "SART_Glo", "SART_Dir", "SART_Dif")
+    df = df %>%
+      mutate(Hour = paste0(substr(DateTime, 1, 4),
+                           "-", substr(DateTime, 5, 6),
+                           "-", substr(DateTime, 7, 8),
+                           " ", substr(DateTime, 9, 10))) %>%
+      mutate(Min = paste0("Min_", substr(DateTime, 11, 12))) %>%
+      mutate(SART_Glo = as.numeric(SART_Glo)) %>%
+      mutate(SART_Dir = as.numeric(SART_Dir)) %>%
+      mutate(SART_Dif = as.numeric(SART_Dif)) %>%
+      select(-DateTime) %>%
+      relocate(Station, Hour, Min, .before = everything())
+    #Station, Hour, Min, SART_Glo, SART_Dir, SART_Dif
+    colnames(df) = c("Station", "Hour", "Min", "Global Solar Radiation (W/m2)", "Direct Radiation (W/m2)", "Diffuse Radiation (W/m2)")
+  }
+
+  if(type == "temp"){
+    #Date time,Automatic Weather Station,Air Temperature(degree Celsius)
+    colnames(df) = c("DateTime", "Station", "TMEP")
+    df = df %>%
+      mutate(Hour = paste0(substr(DateTime, 1, 4),
+                           "-", substr(DateTime, 5, 6),
+                           "-", substr(DateTime, 7, 8),
+                           " ", substr(DateTime, 9, 10))) %>%
+      mutate(Min = paste0("Min_", substr(DateTime, 11, 12))) %>%
+      mutate(TEMP = as.numeric(TEMP)) %>%
+      select(-DateTime) %>%
+      relocate(Station, Hour, Min, .before = everything())
+    #Station, Hour, Min, TEMP
+    colnames(df) = c("Station", "Hour", "Min", "Air Temperature (°C)")
+  }
+
+
+  if(type == "tide"){
+    colnames(df) = c("Station", "Date", "Time", "TIDE")
+    df = df %>%
+      mutate(TIDE = as.numeric(TIDE)) %>%
       mutate(newDate = paste(Date, substr(Time, 1, 2))) %>%
       mutate(newTime = paste0("Min_", substr(Time, 4,5))) %>%
       select(-Date, -Time) %>%
       rename(Hour = newDate, Min = newTime) %>%
       relocate(Station, Hour, Min, .before = everything())
+    #Station, Hour, Min, TIDE
     colnames(df) = c("Station", "Hour", "Min", "Tidal Height (m)")
   }
-
-  #North       = 360
-  #Northwest   = 315
-  #West        = 270
-  #Southwest   = 225
-  #South       = 180
-  #Southeast   = 135
-  #East        = 90
-  #Northeast   = 45
-  #Variable    = -10
-  #Calm        = -20
-  #N/A
 
   if(type == "wind"){
     colnames(df) = c("DateTime", "Station", "Direction", "Wind", "Gust")
     df = df %>%
-      mutate(newDate = paste0(substr(DateTime, 1, 4),
-                              "-", substr(DateTime, 5, 6),
-                              "-", substr(DateTime, 7, 8),
-                              " ", substr(DateTime, 9, 10))) %>%
-      mutate(newTime = paste0("Min_", substr(DateTime, 11, 12))) %>%
+      mutate(Hour = paste0(substr(DateTime, 1, 4),
+                           "-", substr(DateTime, 5, 6),
+                           "-", substr(DateTime, 7, 8),
+                           " ", substr(DateTime, 9, 10))) %>%
+      mutate(Min = paste0("Min_", substr(DateTime, 11, 12))) %>%
       mutate(Direction = ifelse(Direction == "North",     360,
                          ifelse(Direction == "Northwest", 315,
                          ifelse(Direction == "West",      270,
@@ -69,8 +153,9 @@ obj_create = function(rDIR, type){
       mutate(Wind = as.numeric(Wind)) %>%
       mutate(Gust = as.numeric(Gust)) %>%
       select(-DateTime) %>%
-      rename(Hour = newDate, Min = newTime) %>%
       relocate(Station, Hour, Min, .before = everything())
+    #Station, Hour, Min, Direction, Wind, Gust
+    colnames(df) = c("Station", "Hour", "Min", "Mean Direction", "10-min mean wind speed (km/h)", "10-min max gust speed (km/h)")
   }
 
   df = df %>%
@@ -85,7 +170,8 @@ obj_create = function(rDIR, type){
   df = as.data.frame(df)
   obj = list(type = type,
              legend = list("all"),
-             data   = list(df))
-
+             data   = list(df),
+             pivot  = list("XXXX"))
+  class(obj) = "hkweather"
   obj
 }
